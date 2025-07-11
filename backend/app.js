5ae9977e66ea61b140d14f5dd5344efbd6200434
suresh kumar <sk6006061464@gmail.ocm>
2025-07-12 04:07:46 +0530

    cors2

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 
 import express from 'express';
 import morgan from 'morgan';
-
+import cors from 'cors';
 // Routes
 import authRoutes from './routes/authRoutes.js';
 import propertyRoutes from './routes/propertyRoutes.js';
@@ -10,9 +10,13 @@ import leadRoutes from './routes/leadRoutes.js';
 const app = express();
 
 // Middleware
-
+app.use(cors({
+  origin: ['https://trust-well-developers.vercel.app', 'http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 
 // Route Mounting
--- backend/server.js
+++ backend/server.js
@@ -2,26 +2,9 @@
 import app from './app.js';
 import {connectDB} from './db/connectMongo.js';
 import dotenv from 'dotenv';
-import cors from 'cors';
 
 dotenv.config();
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://trust-well-developers.vercel.app',
-  'https://www.trust-well-developers.vercel.app'
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
 
 const PORT = process.env.PORT || 5000;
 
